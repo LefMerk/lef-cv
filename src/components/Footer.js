79e2dc660f25d5127ab815c669aba12ae3e0b927
Eleftherios Merkouriou <lef_merk@yahoo.gr>
2023-11-01 10:06:32 +0200

    fix: footer bg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ import { IoMdMail } from "react-icons/io";
 
 export default function Footer() {
     return (
-        <footer className="aspect-[10/1] font-medium animate__animated animate__fadeInUp bg-[url('../assets/footer_bg.svg')] bg-cover bg-bottom bg-no-repeat">
+        <footer className="aspect-[3/2] md:aspect-[7/1] xl:aspect-[12/1] font-medium animate__animated animate__fadeInUp bg-[url('../assets/footer_bg.svg')] bg-cover bg-bottom bg-no-repeat">
             <div className="max-w-6xl h-full mx-auto text-white text-sm">
                 {/* <div className="h-0.5 w-full bg-gray-700 md:mb-5"></div> */}
-                <div className="flex flex-col-reverse h-full space-y-4 md:space-y-1 md:flex-row justify-between items-center gap-3 px-4 pb-5">
+                <div className="flex flex-col-reverse h-full space-y-4 md:space-y-1 md:flex-row md:justify-between items-center gap-3 px-4 pb-3">
                     <div className="md:self-end">
                         <span>&copy; {new Date().getFullYear()} Eleftherios Merkouriou</span>
                     </div>
